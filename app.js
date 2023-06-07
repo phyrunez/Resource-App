e7f978b9b95a1bcaa6390c8fb4aab2be95bb3cc1
Jude <jude.eze@smartflowtech.com>
2023-06-07 08:45:08 -0400

    11 - using native event object

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
+            name: ''
         };
     },
     methods: {
+        setName(e, lastName) {
+            this.name = `${e.target.value} ${lastName}`
+            // this.name = e.target.value + ' ' + lastName
+        },
         addUp(num) {
             this.counter = this.counter + num
         },
