9025d7a1694cd50fe7b637a1ddb00a1fc4b3488a
Jude <jude.eze@smartflowtech.com>
2023-06-07 09:41:42 -0400

    14 - Assignment_One_Event_Binding

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 10,
+            description: 'Learning Events Binding!',
             name: '',
-            confirmName: ''
-        };
+            anotherName: ''
+        }
     },
     methods: {
-        submitForm() {
-            alert('Submitted!')
+        showAlert() {
+            alert(this.description)
         },
-        setName(e, lastName) {
-            this.name = `${e.target.value} ${lastName}`
-            // this.name = e.target.value + ' ' + lastName
+        changeOutput(e) {
+           this.name = e.target.value 
         },
-        confirmedName() {
-            this.confirmName = this.name
-        },
-        addUp(num) {
-            this.counter = this.counter + num
-        },
-        reduce(num) {
-            this.counter = this.counter - num
-        } 
+        changeAnotherNameOutput(e) {
+            this.anotherName = e.target.value
+        }
     }
-});
-
-app.mount('#events');
+})
+app.mount('#assignment')
