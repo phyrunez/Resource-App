59735c1c43c7cce5d6db2ed62c17097955d9fa92
Jude <jude.eze@smartflowtech.com>
2023-06-07 06:36:26 -0400

    06 - working with data inside a vue app

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             startedVue: 'started learning Vue',
+            masteredVue: 'mastered learning Vue',
             vueLink: 'https://vuejs.org'
         }
     },
     methods: {
         outputGoal() {
             const randomNumber = Math.random()
-            if(randomNumber < 0.5) return 'Learn Vue!'
-            else return 'Master Vue!'
+            if(randomNumber < 0.5) return this.startedVue
+            else return this.masteredVue
         }
     }
 })
