cfff0530713fe8e0eda5482a789fa775b78f59bf
Jude <jude.eze@smartflowtech.com>
2023-06-07 09:10:57 -0400

    12 - exploring Event modifiers

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
-            name: ''
+            name: '',
+            confirmName: ''
         };
     },
     methods: {
+        submitForm() {
+            alert('Submitted!')
+        },
         setName(e, lastName) {
             this.name = `${e.target.value} ${lastName}`
             // this.name = e.target.value + ' ' + lastName
+        },
+        confirmedName() {
+            this.confirmName = this.name
         },
         addUp(num) {
             this.counter = this.counter + num
