2917c7635e2386064dbdcfc4f1cf48cdf885afd8
Jude <jude.eze@smartflowtech.com>
2023-06-06 20:25:47 -0400

    02 - Rebuilding_the_app_with_vue

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const btnEl = document.querySelector('#btn_add-goal')
-const inputEl = document.querySelector('#goal')
-const unorderedListEl = document.querySelector('#list')
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredVal: ''
+        }
+    },
+    methods: {
+        addGoals() {
+            this.goals.push(this.enteredVal)
+            this.enteredVal = ''
+        }
+    }
+}).mount('#app')
 
-btnEl.addEventListener('click', () => {
-    const inputElValue = inputEl.value
-    const listEl = document.createElement('li')
-    listEl.textContent = inputElValue
-    unorderedListEl.appendChild(listEl)
-    console.log(listEl)
-    inputEl.value = ''
-})
+// const btnEl = document.querySelector('#btn_add-goal')
+// const inputEl = document.querySelector('#goal')
+// const unorderedListEl = document.querySelector('#list')
+
+// btnEl.addEventListener('click', () => {
+//     const inputElValue = inputEl.value
+//     const listEl = document.createElement('li')
+//     listEl.textContent = inputElValue
+//     unorderedListEl.appendChild(listEl)
+//     console.log(listEl)
+//     inputEl.value = ''
+// })
 
