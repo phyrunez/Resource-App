2878b7a4a77266c6ec936c5af2477fd5c2384ec6
Jude <jude.eze@smartflowtech.com>
2023-06-07 05:43:59 -0400

    03 - Data_and_Interpolation

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredVal: ''
-        }
-    },
-    methods: {
-        addGoals() {
-            this.goals.push(this.enteredVal)
-            this.enteredVal = ''
+            startedVue: 'started learning Vue'
         }
     }
-}).mount('#app')
-
-// const btnEl = document.querySelector('#btn_add-goal')
-// const inputEl = document.querySelector('#goal')
-// const unorderedListEl = document.querySelector('#list')
-
-// btnEl.addEventListener('click', () => {
-//     const inputElValue = inputEl.value
-//     const listEl = document.createElement('li')
-//     listEl.textContent = inputElValue
-//     unorderedListEl.appendChild(listEl)
-//     console.log(listEl)
-//     inputEl.value = ''
-// })
+})
+app.mount('#user-goal')
 
