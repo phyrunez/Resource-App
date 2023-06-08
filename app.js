43e2cf1b2c5fb8d95837ae5da7474f84418515a5
Jude <jude.eze@smartflowtech.com>
2023-06-08 07:37:46 -0400

    24 - v-if, v-else-if, v-else

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            enteredVal: '',
-            state: false,
-            enteredColor: ''
-        }
-    },
-    computed: {
-        userClasses() {
-            if(this.enteredVal === 'user1') {
-                return { user1: true }
-            }else if(this.enteredVal === 'user2') {
-                return { user2: true }
-            }
-        }
-    },
-    methods: {
-        changeState() {
-            this.state = !this.state
-            console.log(this.state)
-        }
+  data() {
+    return { 
+        goal: '',
+        goals: [] 
+    };
+  },
+  methods: {
+    addGoal() {
+        this.goals.push(this.goal)
+        console.log(this.goals)
     }
-})
-app.mount("#assignment")
+  }
+});
+
+app.mount('#user-goals');
