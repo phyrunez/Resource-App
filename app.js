f38b2e2e303a29a4e7c489fb4c6dffb9515b1e78
Jude <jude.eze@smartflowtech.com>
2023-06-08 08:19:17 -0400

    25 - working with v-for

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return { 
         goal: '',
-        goals: [] 
+        goals: [],
+        dummyObj: { name: 'Jude', age: 24 } 
     };
   },
   methods: {
