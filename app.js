24a5b8c0fce4c05f455130aabce271b0cacdca61
Jude <jude.eze@smartflowtech.com>
2023-06-07 23:20:19 -0400

    18 - Assignment_three_Reactivity

--- app.js
+++ app.js
@@ -1,49 +1,30 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: '',
-      lastName: '',
-      fullname: ''
-    };
-  },
-  watch: {
-    name(value) {
-        if(value === '') this.fullname = ''
-        this.fullname = value + ' ' + this.lastName
+    data() {
+        return {
+            result: 0,
+        }
     },
-    lastName(value) {
-        if(value === '') this.name = ''
-        this.fullname = this.name + ' ' + value
+    watch: {
+        result(value) {
+            if(value++) {
+                setTimeout(() => {
+                    value = 0
+                }, 5000)
+            }
+            console.log(value)
+        }
+    },
+    computed: {
+        displayMessage() {
+            if(this.result <= 37) return 'Not there yet'
+            if(this.result > 37) return 'Too Much!'
+        }
+    },
+    methods: {
+        addUp(num) {
+            this.result = this.result + num
+            console.log(this.result)
+        }
     }
-  },
-  computed: {
-    // fullname() {
-    //     console.log('Running everytime again')
-    //     if(this.name === '') return ''
-    //     return this.name + ' ' + this.lastName
-    // }
-  },
-  methods: {
-    outputFullname(lastName) {
-        console.log('Running everytime')
-        if(this.name === '') return ''
-        return this.name + " " + lastName
-    },
-    setName(event) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput() {
-        this.name = ''
-    }
-  }
-});
-
-app.mount('#events');
+})
+app.mount('#assignment')
