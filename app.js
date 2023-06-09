765e76c2124560db056689e8c9ab734bc83f58f3
Jude <jude.eze@smartflowtech.com>
2023-06-09 15:06:36 -0400

    31 - Vue Lifecycle Basic

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    data() {
+        return {
+        currentUserInput: '',
+        message: 'Vue is great!',
+        };
     },
-    setText() {
-    //   this.message = this.currentUserInput;
-        this.message = this.$refs.userInput.value
-        console.dir(this.$refs.userInput)
-        console.log(typeof(this.$refs.userInput))
+    methods: {
+        saveInput(event) {
+            this.currentUserInput = event.target.value;
+        },
+        setText() {
+            //   this.message = this.currentUserInput;
+            this.message = this.$refs.userInput.value
+            console.dir(this.$refs.userInput)
+            console.log(typeof(this.$refs.userInput))
+        },
     },
-  },
+    beforeCreate() {
+        alert('Before Create!!')
+    },
+    created() {
+        alert('Created!!')
+    },
+    beforeMount() {
+        alert('Before Mount!!')
+    },
+    mounted() {
+        alert('Mounted!!')
+    },
+    beforeUpdate() {
+        alert('Before Update!!')
+    },
+    updated() {
+        alert('Updated!!')
+    },
+    beforeUnmount() {
+        alert('Before Unmount!!')
+    },
+    unmounted() {
+        alert('Unmounted!!')
+    },
 });
 
 app.mount('#app');
 
-const app2 = Vue.createApp({
-  template: `
-    <p>{{ favouriteMeal }}</p>
-  `,
-  data() {
-    return {
-      favouriteMeal: 'Rice'
-    };
-  },
-  methods: {
-    
-  },
-});
-
-app2.mount('#app2');
