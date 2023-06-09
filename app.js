2ae15375c2e9ebb95b23a60dac36070e7c2b0432
Jude <jude.eze@smartflowtech.com>
2023-06-09 09:25:54 -0400

    30 - template and refs

--- app.js
+++ app.js
@@ -10,29 +10,28 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+    //   this.message = this.currentUserInput;
+        this.message = this.$refs.userInput.value
+        console.dir(this.$refs.userInput)
+        console.log(typeof(this.$refs.userInput))
     },
   },
 });
 
 app.mount('#app');
 
-const data = {
-    message: 'Hello!',
-    logMessages: 'Hello! World!'
-};
+const app2 = Vue.createApp({
+  template: `
+    <p>{{ favouriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favouriteMeal: 'Rice'
+    };
+  },
+  methods: {
+    
+  },
+});
 
-const handler = {
-    set(target, key, value) {
-        console.log(target)  //displays the data object
-        console.log(key)  //displays the key of the object == message
-        console.log(value)  //displays what we set as the value of the key property  => name = 'anything'
-
-        if(key === 'message') target.logMessages = value + ' World!'
-        target.message = value
-    }
-}
-
-const proxy = new Proxy(data, handler);
-proxy.message = 'Helloo!!!!!'
-console.log(proxy.logMessages)
+app2.mount('#app2');
