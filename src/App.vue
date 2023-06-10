6d0e37476a2e367a26f9f312350dfab364534ce3
Jude <jude.eze@smartflowtech.com>
2023-06-10 13:47:01 -0400

    Props and Emitting events assignment

--- src/App.vue
+++ src/App.vue
@@ -1,136 +1,29 @@
 <template>
-  <section>
-    <header>
-      <h1>My Friends</h1>
-    </header>
-    <new-friend
-      @added-contact="addedNewContact"
-      ></new-friend>
-    <ul>
-      <friend-contact 
-        v-for="friend in friends" 
-        :key="friend.id"
-        :id="friend.id"
-        :name="friend.name"
-        :phone="friend.phone"
-        :email="friend.email"
-        :is-favourite="friend.isFavourite"
-        @toggle-favourite="toggleFavStatus"
-        @delete="deleteContact"
-      ></friend-contact>
-      <!-- <friend-contact></friend-contact> -->
-    </ul>
-  </section>
+    <div>
+        <user-data
+            @update-user-data="updateUserData"
+        ></user-data>
+        <active-user
+            :userDataObj="userDataObj"
+        ></active-user>
+    </div>
 </template>
 
 <script>
-import FriendContact from "./components/FriendContact.vue"
-import NewFriend from "./components/NewFriend.vue"
-
 export default {
-  components: {
-    "friend-contact" : FriendContact,
-    "new-friend": NewFriend,
-  },
-  data() {
-    return {
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
-          isFavourite: true
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "0987 654421 21",
-          email: "julie@localhost.com",
-          isFavourite: false
-        },
-      ],
-    };
-  },
-  methods: {
-    toggleFavStatus(friendId) {
-      const identifiedFriend = this.friends.find(friend => friend.id === friendId)
-      identifiedFriend.isFavourite = !identifiedFriend.isFavourite
+    data() {
+        return {
+            userDataObj: { name: "Jude", age: 24 }
+        }
     },
-    addedNewContact( name, phone, email ) {
-      const newContactAdded = {
-        id: new Date().toISOString(),
-        name: name,
-        phone: phone,
-        email: email,
-        isFavourite: false
-      }
-      this.friends.push(newContactAdded)
-      console.log(this.friends)
-    },
-    deleteContact(friendId) {
-      this.friends = this.friends.filter(friend => friend.id !== friendId)
-      console.log(this.friends)
+    methods: {
+        updateUserData(name, age) {
+            this.userDataObj = {
+                name: name,
+                age: age
+            }
+            console.log(this.userDataObj)
+        }
     }
-  }
-};
+}
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-html {
-  font-family: "Jost", sans-serif;
-}
-body {
-  margin: 0;
-}
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-#app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-#app li,
-#app form {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-#app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-</style>
